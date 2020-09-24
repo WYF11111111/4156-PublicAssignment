13c791431ed7c0a5606c5f47492d691c3751ee2f
WYF11111111 <yw3471@columbia.edu>
2020-09-25 01:23:15 +0800

    finished!!!!!!!!

--- hw1/src/main/java/models/Move.java
+++ hw1/src/main/java/models/Move.java
@@ -8,4 +8,36 @@ public class Move {
 
   private int moveY;
 
+  //getter of the player of a specific move
+  public Player getPlayer() {
+	return player;
+  }
+
+  //setter of the player of a specific move
+  public void setPlayer(Player player) {
+	this.player = player;
+  }
+
+  //getter of the move in x coordinate
+  public int getMoveX() {
+	return moveX;
+  }
+
+  //setter of the move in x coordinate
+  public void setMoveX(int moveX) {
+	this.moveX = moveX;
+  }
+
+  //getter of the move in y coordinate
+  public int getMoveY() {
+	return moveY;
+  }
+
+  //setter of the move in y coordinate
+  public void setMoveY(int moveY) {
+	this.moveY = moveY;
+  }
+
 }
+
+
--- hw1/src/main/java/models/Player.java
+++ hw1/src/main/java/models/Player.java
@@ -6,4 +6,28 @@ public class Player {
 
   private int id;
 
+  //Constructor
+  public Player(){
+    this.type = ' ';
+    this.id = 0;
+  }
+  
+  //getter and setter of type and id
+  public char getType() {
+	return type;
+  }
+
+  public void setType(char type) {
+	this.type = type;
+  }
+
+  public int getId() {
+	return id;
+  }
+
+  public void setId(int id) {
+	this.id = id;
+  }
+  
 }
+
